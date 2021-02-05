de5a9b05c004524fb4bd6a97bbe556cdb9817001
arunsahu222 <70147260+arunsahu222@users.noreply.github.com>
2021-02-05 15:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ def predict():
     result = model.predict(data_df)
 
     # send back to browser
-    output = {'results': int(result[0])}
+    output = {'Can be Converted': int(result[0])}
 
     # return data
-    return jsonify(results=output)
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
